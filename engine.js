e2d9e8c9aa19cc17b273c86f85d0a9db694515ad
marcodome <marco.dome@yahoo.it>
2024-01-25 11:42:33 +0100

    feat: adds command history

--- engine.js
+++ engine.js
@@ -1,15 +1,38 @@
 // main.js
 // when document is ready and loaded do stuffs....
 $(document).ready(function () {
+
+    let commandHistory = [];
+    let commandHistoryIndex = -1;
+
     document.getElementById('input').addEventListener('keydown', function (event) {
-        if (event.key === 'Enter' && !event.shiftKey) {
-          event.preventDefault();
-          processCommand(this.value);
-          this.value = '';
+      // process the command typed in the input field when enter key is pressed  
+      if (event.key === 'Enter' && !event.shiftKey) { // if enter key is pressed and shift is not pressed
+          event.preventDefault(); // prevent default behaviour of enter key
+          commandHistoryIndex = -1; // reset command history index
+          processCommand(this.value); // process the command and return output to video
+          this.value = ''; // clean the text area input
+        } 
+        // scroll the command history up
+        else if (event.key === 'ArrowUp') {
+          event.preventDefault(); // prevent default behaviour of enter key
+          if(commandHistory.length === 0) return; // if command history is empty, do nothing
+          if(commandHistoryIndex + 1 >= commandHistory.length) return; // if we have reached the top of history, do nothing (out of bounds
+          commandHistoryIndex++; // increment command history index
+          this.value = commandHistory[commandHistoryIndex]; // get the last command from history
+        } 
+        // scroll the command history down
+        else if (event.key === 'ArrowDown') {
+          event.preventDefault(); // prevent default behaviour of enter key
+          if(commandHistory.length === 0) return; // if command history is empty, do nothing
+          if(commandHistoryIndex - 1 < -1) return; // if we have reached the top of history, do nothing (out of bounds)
+          commandHistoryIndex--; // increment command history index
+          this.value = commandHistoryIndex >= 0? commandHistory[commandHistoryIndex] : ''; 
         }
       });
 
       function processCommand(command) {
+        commandHistory.unshift(command);
         // Simulate command processing here
         let outputSection = document.getElementById('output');
         // outputSection.textContent += '> ' + command + '\nOutput: Command executed successfully\n\n';
